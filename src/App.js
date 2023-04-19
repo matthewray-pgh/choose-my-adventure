68e062ec4270f8b3cc0a633c37e2bf300d202463
Matthew <mray@zoll.com>
2023-04-18 20:31:29 -0400

    feature: add lost key basic story flow and styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './styles/App.scss';
 
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { LostKey } from './pages/LostKey';
 
 function App() {
   return (
     <MemoryRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/LostKey" element={<LostKey />} />
       </Routes>
     </MemoryRouter>
   );
